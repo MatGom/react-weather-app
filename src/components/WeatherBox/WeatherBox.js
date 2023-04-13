84ccb37295141a0d83c33b6e6bcfa9e5cfd5014e
Mateusz Gomolka <mdm.gomolka@gmail.com>
2023-04-13 01:06:36 +0100

    Prepare handleCityChange function

--- src/components/WeatherBox/WeatherBox.js
+++ src/components/WeatherBox/WeatherBox.js
@@ -1,15 +1,20 @@
 import PickCity from '../PickCity/PickCity';
 import WeatherSummary from '../WeatherSummary/WeatherSummary';
 import Loader from '../Loader/Loader';
+import { useCallback } from 'react';
 
 const WeatherBox = props => {
+  const handleCityChange = useCallback(city => {
+    console.log(city);
+  }, []);
+
   return (
     <section>
-      <PickCity />
+      <PickCity action={handleCityChange} />
       <WeatherSummary />
       <Loader />
     </section>
-  )
+  );
 };
 
 export default WeatherBox;
