adc8fc5e7f75a75fc56a3b9653cb1b683076b547
Mateusz Gomolka <mdm.gomolka@gmail.com>
2023-04-13 01:16:10 +0100

    Add fetch and weatherData object to handleCityChange

--- src/components/WeatherBox/WeatherBox.js
+++ src/components/WeatherBox/WeatherBox.js
@@ -5,7 +5,19 @@ import { useCallback } from 'react';
 
 const WeatherBox = props => {
   const handleCityChange = useCallback(city => {
-    console.log(city);
+    fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=f2c045376a4babd0896cc0e03fa4b5a3&units=metric`
+    )
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        const weatherData = {
+          city: data.name,
+          temp: data.main.temp,
+          icon: data.weather[0].icon,
+          description: data.weather[0].main,
+        };
+      });
   }, []);
 
   return (
